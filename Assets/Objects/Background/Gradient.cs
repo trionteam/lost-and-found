2d0095ac0ffa540d050917fcdb5b1c64df379aba
Ondrej Sykora <mail@ondrejsykora.com>
2021-02-28 22:53:05 +0100

    Add more graphics.

---
+++ Assets/Objects/Background/Gradient.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Gradient : MonoBehaviour
+{
+    private MeshFilter _meshFilter;
+
+    [SerializeField]
+    private Color _bottomColor;
+    [SerializeField]
+    private Color _topColor;
+
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        Debug.Assert(_meshFilter != null);
+    }
+
+    void Start()
+    {
+        var mesh = _meshFilter.mesh;
+        var vertices = mesh.vertices;
+        var minY = vertices.Select(vertex => vertex.y).Min();
+        var maxY = vertices.Select(vertex => vertex.y).Max();
+        Color[] colors = new Color[vertices.Length];
+        for (int i = 0; i < vertices.Length; ++i)
+        {
+            float relativeY = (vertices[i].y - minY) / maxY;
+            colors[i] = Color.Lerp(_bottomColor, _topColor, relativeY);
+        }
+        mesh.colors = colors;
+    }
+}
